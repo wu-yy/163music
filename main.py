58cfd9117100d904b9936a0f144582182d96752d
蓝鲸123 <wuyongyu13@126.com>
2017-12-14 23:20:49 +0800

    生成词云

---
+++ main.py
@@ -0,0 +1,7 @@
+from mywordCloud import save_jieba_result
+from mywordCloud import draw_wordcloud
+
+
+if __name__=='__main__':
+    save_jieba_result('anheqiao.txt')
+    draw_wordcloud('pjl_jieba.txt')
